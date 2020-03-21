c9abec396a23f41f0eb4e7e46d8a914e8ab1206a
4bselyam <45922762+4bselyam@users.noreply.github.com>
2020-03-21 20:32:32 +0200

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -6,17 +6,21 @@ $.fn.weather = function (options) {
         slideSpeed: 400
     }, options);
 
-    if (params.key.length == 0) $('.input').find('h3').html("You've forgotten to write your <a href='https://home.openweathermap.org/api_keys'>API key</a>").css('color', 'red');
+    if (!params.key)
+        $('.input').find('h3')
+            .html("You've forgotten to write your <a href='https://home.openweathermap.org/api_keys'>API key</a>")
+            .css('color', '#e74c3c');
 
-    var $card = $('.card');
-    var $result = $card.find('.result');
-    var city;
+    var $card = $('.card'),
+        $result = $card.find('.result'),
+        city = 0;
 
     // ---------Events---------
 
     // Обработчик enter
     $('body').keypress(function (event) {
-        city = $.trim($card.find('input[name=city]').val());
+        city = $card.find('input[name=city]').val();
+        city = $.trim(city);
         if (event.which == 13) {
             if (city.length == 0) $('.input').find('h3').html('The input is empty!');
             json(city);
@@ -47,7 +51,8 @@ $.fn.weather = function (options) {
                 console.error(error.responseJSON['cod'] + ' ' + error.responseJSON['message']);
                 if (error.responseJSON['cod'] == 404) {
                     $('.input').find('h3').html("There isn't such city!");
-                }
+                } else if (error.responseJSON['cod'] == 401)
+                    $('.input').find('h3').html("Your API key is wrong!");
             });
     }
 
@@ -56,7 +61,18 @@ $.fn.weather = function (options) {
         $card.addClass('active');
 
         // Информация про город, чистоту неба и температура
-        $result.html('<h3>' + data['city']['name'] + ', ' + data['city']['country'] + '</h3>');
+        city = data['city']['name'];
+        $result.html('<h3></h3>');
+
+        if (city.length >= 10) {
+            for (i = 0; i < 8; i++) {
+                $result.find('h3').append(city[i]);
+            }
+            $result.find('h3').append('..,' + data['city']['country']);
+        } else {
+            $result.html('<h3>' + data['city']['name'] + ', ' + data['city']['country'] + '</h3>');
+        }
+
         $result.append('<h1>' + Math.round(data['list'][0]['main']['temp']) + '°<span></span></h1>');
         $result.append('<p class="desc">' + data['list'][0]['weather'][0]['main'] + '</p>');
 
@@ -75,51 +91,53 @@ $.fn.weather = function (options) {
 
     // Прогноз на перёд (по часам тоже)
     function day(data) {
-        var temp = [];
-        var img = [];
-        var min_temp = [];
-        var max_temp = [];
-        var wind = []
+        var temp = [],
+            img = [],
+            min_temp = [],
+            max_temp = [],
+            wind = [],
+            hourTemp = [],
+            hourIcon = [],
 
-        var hourTemp = [];
-        var hourIcon = [];
+            // Дата для прогноза по часам
+            dateTxt = [],
+            dayTxt = [],
+            hourTxt = ['00:00', '03:00', '06:00', '09:00', '12:00', '15:00', '18:00', '21:00'],
+            hourForecast = [{
+                day: 1
+            }];
+
+        var nowMonth = (new Date().getMonth() + 1).toString(),
+            nowYear = new Date().getFullYear();
+
+        nowMonth.length == 1 ? nowMonth = (0).toString() + nowMonth.toString() : nowMonth = nowMonth;
 
         // Дата для прогноза на 5 вперед 
-        var nowDay = new Date().getDate();
-        var nowMonth = new Date().getMonth();
-        var nowYear = new Date().getFullYear();
+        var nowDay = new Date().getDate().toString();
+        nowDay.length == 1 ? nowDay = (0).toString() + nowDay.toString() : nowDay = nowDay;
 
-        // Дата для прогноза по часам
-        var dateSplit = [];
-        var dateTxtSplit = [];
-        var dayTxt = [];
-        var hourTxt = [];
-        var hourForecast = [];
 
         $result.append('<ul></ul>');
 
         // Получаю дату, часы и данные о погоде
         for (i = 0; i < data['cnt']; i++) {
-            var date = data['list'][i]['dt_txt'];
-            dateSplit.push(date.split('-', 3));
-            dateTxtSplit.push(dateSplit[i][2].split(' ', 2));
+            var date = data['list'][i]['dt_txt'],
+                currDate = nowYear + '-' + nowMonth + '-' + nowDay;
+            dateTxt.push(date.split(' ', 2));
+            dateTxt[i].splice(1);
+            dateTxt[i] = dateTxt[i][0];
 
             // Убираю всё, что связано с сегодняшним днём
-            if (dateTxtSplit[i][0] == nowDay) {
-                dateTxtSplit[i].splice(dateTxtSplit[i][0].indexOf(nowDay));
-            }
+            // if (dateTxt[i].includes(currDate)) {
+            // dateTxt.splice(dateTxt.indexOf(currDate))
+            // }
 
             // Убираем "пустые" элементы
-            dayTxt.push(dateTxtSplit[i][0]);
-            dayTxt = dayTxt.filter(function (x) {
-                return x !== undefined && x !== null;
-            });
 
-            hourTxt.push(dateTxtSplit[i][1]);
-            hourTxt = hourTxt.filter(function (x) {
-                return x !== undefined && x !== null;
-            });
-            hourTxt.splice(8);
+            // dayTxt.push(dateTxtSplit[i][0]);
+            // dayTxt = dayTxt.filter(function (x) {
+            //     return x !== undefined && x !== null;
+            // });
 
             // Получаю данные о погоде
             if (date.includes('12:00:00')) {
@@ -130,43 +148,40 @@ $.fn.weather = function (options) {
                 img.push(data['list'][i]['weather'][0]['icon']);
             }
 
-            for (q = 0; q < hourTxt.length; q++) {
-                if (date.includes(hourTxt[q]) && date.includes(dayTxt[q])) {
-                    hourTemp.push(Math.round(data['list'][i]['main']['temp']) + '&deg;C');
-                    hourIcon.push(data['list'][i]['weather'][0]['icon']);
-                }
+            if ((data['list'][i]['dt_txt']).includes(dateTxt[i])) {
+                hourTemp.push(data['list'][i]['main']['temp'])
             }
         }
 
-        console.log(hourIcon, hourTemp)
-        noCopy(dayTxt);
+        console.log(hourTemp);
+
 
         // Генерирую данные на странице
         for (i = 0; i < temp.length; i++) {
-            var nowDay = new Date().getDate() + 1 + i;
-            var nowStringDate = new Date(nowYear, nowMonth, nowDay).toDateString().split(' ', 2);
+            var nowDay = new Date().getDate() + 1 + i,
+                nowStringDate = new Date(nowYear, nowMonth, nowDay).toDateString().split(' ', 2);
 
-            hourForecast.push({ day: dayTxt[i], hour: [hourTxt] })
-
-            $result.find('ul').append('<li>' + nowStringDate[0] + '<p class="feature"><img src="https://openweathermap.org/img/wn/' + img[i] + '@2x.png" width="25" alt="weather icon">' + temp[i] + '</p><p class="other">Min: ' + min_temp[i] + ' Max: ' + max_temp[i] + '<span>Wind speed: ' + wind[i] + '</span></p></li>');
-            $result.find('ul').append('<div class="temp-hourly"></div>');
+            $result.find('ul').append('<li>' + nowStringDate[0] + '<p class="feature"><img src="https://openweathermap.org/img/wn/' + img[i] + '@2x.png" width="25" alt="weather icon">'
+                + temp[i] + '</p><p class="other">Min: ' + min_temp[i]
+                + ' Max: ' + max_temp[i]
+                + '<span>Wind speed: ' + wind[i] + '</span></p></li><div class="temp-hourly"></div>');
         }
 
         // Вставляю часы в список
-        for (k = 0; k < hourTxt.length; k++) {
-            $result.find('ul .temp-hourly').append('<p>' + hourTxt[k].substring(0, hourTxt[k].length - 3) + ':<img src="https://openweathermap.org/img/wn/' + hourIcon[k] + '@2x.png" width="45" alt="weather icon"><span>' + hourTemp[k] + '</span></p>');
+        for (i = 0; i < hourTxt.length; i++) {
+            $result.find('ul .temp-hourly').append('<p>' + hourTxt[i] + '</p>');
         }
 
         // Клик для открытия блока "по часам"
         $result.find('ul .temp-hourly').hide();
         $result.find('ul li').click(function () {
-            var findDiv = $(this).next('.temp-hourly');
-            var findWrapper = $(this).closest('ul');
+            var findDiv = $(this).next('.temp-hourly'),
+                findWrapper = $(this).closest('ul');
 
             if (findDiv.is(':visible')) {
                 findDiv.slideUp(parseInt(params.slideSpeed));
             } else {
-                findWrapper.find('.temp-hourly').slideUp();
+                findWrapper.find('.temp-hourly').slideUp(parseInt(params.slideSpeed));
                 findDiv.slideDown(parseInt(params.slideSpeed));
             }
         });
